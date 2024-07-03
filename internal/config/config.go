7afbccbfd0926e7356afbbd4dd8faa48b3656b80
Simoffsky <maks-kirsanov26@mail.ru>
2024-07-03 18:02:18 +1000

    Add App configuring

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,16 @@
 package config
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Config struct {
 	LoggerLevel string
 	HTTPTimeout time.Duration
 	ServerPort  string
+}
+
+func (c Config) String() string {
+	return fmt.Sprintf("LoggerLevel: %s, HTTPTimeout: %s, ServerPort: %s", c.LoggerLevel, c.HTTPTimeout, c.ServerPort)
 }
--- internal/todo-app/app.go
+++ internal/todo-app/app.go
@@ -1,7 +1,41 @@
 package todoapp
 
-import "todo/pkg/log"
+import (
+	"time"
+	"todo/internal/config"
+	"todo/pkg/log"
+)
 
 type App struct {
+	config *config.Config
 	logger log.Logger
+}
+
+func NewApp(config *config.Config) *App {
+	return &App{
+		config: config,
+	}
+}
+
+func (a *App) Run() error {
+	err := a.configureApp()
+	if err != nil {
+		return err
+	}
+
+	return a.startHTTPServer()
+}
+
+func (a *App) configureApp() error {
+	a.logger = log.NewDefaultLogger(
+		log.LevelFromString(a.config.LoggerLevel),
+	).WithTimePrefix(time.DateTime)
+
+	a.logger.Debug("Config parameters: " + a.config.String())
+
+	return nil
+}
+
+func (a *App) startHTTPServer() error {
+	return nil
 }
