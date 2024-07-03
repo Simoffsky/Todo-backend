ad55e9c4d3f84d5a8760f8e19e06030c9fb0a429
Simoffsky <maks-kirsanov26@mail.ru>
2024-07-03 18:30:18 +1000

    Add auth proto

---
+++ scripts/proto_gen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd "$(dirname "$0")/.."
+
+mkdir -p ./pkg/proto/auth
+
+protoc  --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    ./pkg/proto/auth/auth.proto
