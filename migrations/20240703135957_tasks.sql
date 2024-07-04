6ccf736eb5afd9dbaa5469d0a20fef734cf78189
Simoffsky <maks-kirsanov26@mail.ru>
2024-07-04 19:31:27 +1000

    Add FK to users from tasks

--- migrations/20240703135957_tasks.sql
+++ migrations/20240703135957_tasks.sql
@@ -3,18 +3,19 @@ BEGIN;
 CREATE TABLE task_lists (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    owner VARCHAR(255) NOT NULL
+    owner VARCHAR(255) REFERENCES users(login) ON DELETE CASCADE,
 );
 
 CREATE TABLE tasks (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    owner VARCHAR(255) NOT NULL,
+    owner VARCHAR(255) REFERENCES users(login) ON DELETE CASCADE,
     body TEXT,
     is_done BOOLEAN DEFAULT FALSE,
     task_list_id INT,
     FOREIGN KEY (task_list_id) REFERENCES task_lists(id) ON DELETE CASCADE
 );
+
 
 COMMIT;
 
